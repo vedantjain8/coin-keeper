922eff7a5da731b02cb92f3c98fe020e662d0cfd
Vedant Jain <vedantjain88666@gmail.com>
2023-07-22 21:43:22 +0530

    fix: settings initial data load

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ A new Flutter project.
 - [x] fix: amount value calculation on updating query
 - [x] add onclick for each transaction and open edit page [to include: title, description, amount, wallet, category, income/expense]
 - [x] settings page [show github profile, change currency, delete all data, account holder name]
+- [x] fix when editing transaction changing wallet causes imbalance of wallet amount
 
 ### Working on it
 
@@ -31,7 +32,7 @@ A new Flutter project.
 - [ ] infinite scroll pagination, lazy loading
 - [ ] fix dbExportToDownloadFolder in sql_helper.dart
 - [ ] fix settings initial value from getting from shared preferences
-- [ ] fix when editing transaction changing wallet causes imbalance of wallet amount
+- [ ] fix currency change [on restart the currency changes to default INR]
 - [ ] fix sql_helper new wallet add amount [total wallet amount calculation]
 - [ ] fix edit_transaction updateItem() values are not updating
 - [ ] add mobile platform validation for screen
@@ -43,3 +44,93 @@ A new Flutter project.
 ### maybe TODOs
 - [ ] add multi user
 - [ ] multiple language support
+
+# GPT3 help
+## How to update formateCurrency from the same file wihout passing the value all over the app seperately
+If you want to avoid passing the `formatCurrency` instance around multiple files, you can consider using a `ChangeNotifier` with `Provider` from the `provider` package to manage the currency state in your app. This way, you can update the currency value from anywhere in your app and automatically propagate the changes to all the widgets that use the currency formatting.
+
+Here's how you can achieve this:
+
+1. Create a `CurrencyProvider` class that extends `ChangeNotifier`. This class will hold the current currency value and provide methods to update it.
+
+```dart
+import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+import 'package:coinkeeper/settings/sharedpreferences.dart';
+
+class CurrencyProvider with ChangeNotifier {
+  String _currencyName = "INR";
+  NumberFormat _currencyFormat = NumberFormat.simpleCurrency(name: "INR");
+
+  CurrencyProvider() {
+    _initCurrency();
+  }
+
+  String get currencyName => _currencyName;
+  NumberFormat get currencyFormat => _currencyFormat;
+
+  Future<void> _initCurrency() async {
+    final currency = await getOption("currency");
+    if (currency != null) {
+      _currencyName = currency;
+      _currencyFormat = NumberFormat.simpleCurrency(name: currency);
+    }
+    notifyListeners();
+  }
+
+  Future<void> setCurrency(String newCurrency) async {
+    _currencyName = newCurrency;
+    _currencyFormat = NumberFormat.simpleCurrency(name: newCurrency);
+    await saveOption("currency", newCurrency);
+    notifyListeners();
+  }
+}
+```
+
+2. Wrap your root widget with the `ChangeNotifierProvider` to make the `CurrencyProvider` accessible throughout the app.
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CurrencyProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+```
+
+3. Now, you can access the `CurrencyProvider` and update the currency value from any widget in your app.
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final currencyProvider = Provider.of<CurrencyProvider>(context);
+    // Access currency name and format from the provider
+    final currencyName = currencyProvider.currencyName;
+    final currencyFormat = currencyProvider.currencyFormat;
+
+    // You can use the currency format to format values
+    final formattedValue = currencyFormat.format(1234.56);
+
+    return Text("Currency: $currencyName, Formatted Value: $formattedValue");
+  }
+}
+```
+
+4. To update the currency value from any widget, use the `setCurrency` method of the `CurrencyProvider`.
+
+```dart
+// Somewhere in your widget tree:
+final currencyProvider = Provider.of<CurrencyProvider>(context, listen: false);
+currencyProvider.setCurrency("USD");
+```
+
+By using this approach, you centralize the currency management logic and can easily update the currency value from anywhere in your app. Widgets that depend on the currency formatting will automatically rebuild and display the updated currency information.
--- lib/screens/popUpMenu/settings.dart
+++ lib/screens/popUpMenu/settings.dart
@@ -15,7 +15,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   // variables
-  String _dropdownValue = _listCurrencies.first;
+  String _dropdownValue = "";
   TextEditingController _nameController = TextEditingController();
   // TODO add the initial value to the _nameController
   // TODO add the initial value of currency from the sharedprefeneces
@@ -35,124 +35,145 @@ class _SettingsPageState extends State<SettingsPage> {
     saveOption('currency', newName);
   }
 
+  Future<void> _updateValues() async {
+    getOption("userName")
+        .then((value) => _nameController = TextEditingController(text: value));
+
+    final currency = await getOption("currency");
+    setState(() {
+      _dropdownValue = currency ?? _listCurrencies.first;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    _nameController =
-        TextEditingController(text: getOption("userName").toString());
+    _updateValues();
   }
 
   @override
   void dispose() {
     _nameController.dispose();
     super.dispose();
+    print("dispose used");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(0.8),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const Text("User Name:"),
-                const SizedBox(
-                  width: 10,
-                ),
-                SizedBox(
-                  // TODO make this width dynamic
-                  width: 220,
-                  child: TextField(
-                    controller: _nameController,
-                    maxLength: 20,
-                    maxLines: 1,
-                    onSubmitted: (value) {
-                      saveOption("userName", value);
-                    },
-                  ),
-                )
-              ],
+    return FutureBuilder(
+      future: getOption("userName"),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show a loading indicator while waiting for the data to be fetched
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          // Once the data is available, build the rest of the UI
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Settings"),
             ),
-            Row(
-              children: [
-                Text(
-                  "Select Currency:",
-                  style: TextStyle(fontSize: globalFontSize),
-                ),
-                ButtonTheme(
-                  alignedDropdown: true,
-                  child: DropdownButton<String>(
-                    value: _dropdownValue,
-                    icon: const Icon(Icons.attach_money),
-                    onChanged: (String? value) {
-                      // This is called when the user selects an item.
-                      setState(() {
-                        _dropdownValue = value!;
-                        updateCurrencyName(_dropdownValue);
-                      });
-                    },
-                    items: _listCurrencies
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Delete all Data:",
-                  style: TextStyle(fontSize: globalFontSize),
-                ),
-                ElevatedButton(
-                  onPressed: () => showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      icon: const Icon(Icons.warning),
-                      iconColor: Colors.red,
-                      title: const Text("Are you sure?"),
-                      content: const SingleChildScrollView(
-                          child: Text(
-                              "By accepting you clear all the data from this app")),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
+            body: Padding(
+              padding: const EdgeInsets.all(0.8),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text("User Name:"),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        // TODO make this width dynamic
+                        width: 220,
+                        child: TextField(
+                          controller: _nameController,
+                          maxLength: 20,
+                          maxLines: 1,
+                          onChanged: (value) {
+                            saveOption("userName", value);
                           },
-                          child: const Text(
-                            "Decline",
-                            style: TextStyle(color: Colors.blue),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Select Currency:",
+                        style: TextStyle(fontSize: globalFontSize),
+                      ),
+                      ButtonTheme(
+                        alignedDropdown: true,
+                        child: DropdownButton<String>(
+                          value: _dropdownValue,
+                          icon: const Icon(Icons.attach_money),
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              _dropdownValue = value!;
+                              updateCurrencyName(_dropdownValue);
+                            });
+                          },
+                          items: _listCurrencies
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Delete all Data:",
+                        style: TextStyle(fontSize: globalFontSize),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            icon: const Icon(Icons.warning),
+                            iconColor: Colors.red,
+                            title: const Text("Are you sure?"),
+                            content: const SingleChildScrollView(
+                                child: Text(
+                                    "By accepting you clear all the data from this app")),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  "Decline",
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  _doomTable();
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  "Accept",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            _doomTable();
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text(
-                            "Accept",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        ),
-                      ],
-                    ),
+                        child: const Text("BOOM BABY"),
+                      ),
+                    ],
                   ),
-                  child: const Text("BOOM BABY"),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
